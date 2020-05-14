6dd1e649939ab92e161565cb1e06890cc30120d3
Regina Dircio <reginadircio@gmail.com>
2020-05-14 13:31:11 -0500

    Import function from one file to another

---
+++ my_lamb_data/my_mod.py
@@ -0,0 +1,11 @@
+def enlarge(n):
+    return n * 100
+
+
+
+if __name__ == "__main__":  # will run only if mentioned from the command line
+    x = 5
+    print(enlarge(x))
+    # get user imput
+    y = int(input('Please choose a number '))
+    print(enlarge(y))
--- my_lamb_data/my_script.py
+++ my_lamb_data/my_script.py
@@ -2,6 +2,7 @@ import pandas
 from pandas import DataFrame
 
 # from my_mod import enlarge
+
 # another way to import if working outside of my_lambdata (most often the case)
 from my_lambdata.my_mod import enlarge
 
@@ -9,3 +10,6 @@ print('Hello')
 
 df = DataFrame({'a':[1,2,3], 'b':[4,5,6]})
 print(df.head())
+
+x = 11
+print(enlarge(x))
