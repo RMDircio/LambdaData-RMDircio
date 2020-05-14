16f77cbbe6c15e298919edc15b385051b78e95b4
Regina Dircio <reginadircio@gmail.com>
2020-05-14 17:14:48 -0500

    Auto class with Trucks

--- my_lamb_data/autos.py
+++ my_lamb_data/autos.py
@@ -10,17 +10,33 @@ class Auto():
 
     def drive(self):
         print("WE ARE DRIVING", self.model)
+    
+    def wash(self):
+        print('Washing the vehicle')    
+
+class Truck(Auto):
+    def __init__(self, make, model, year, color, num_wheels, bed_size=None):
+        super().__init__(make, model, year, color, num_wheels)
+        self.bed_size = bed_size
+    
+    def drive(self):
+        print("WE ARE DRIVING", self.model, 'with bed size:', self.bed_size)
 
 if __name__ == "__main__":
     car = Auto("Toyota", "Prius", 2020, "Blue", 4)
     print(car.make, car.model)
     car.drive()
+    car.wash()
 
     car2 = Auto("Tesla", "Model S", 2020, "Blue", 4)
     car2.drive()
+    car2.wash()
 
-    truck = Auto("Ford", "F150", 2020, "Blue", 4)
+    truck = Truck("Ford", "F150", 2020, "Blue", 4, bed_size='5x5')
     truck.drive()
+    truck.wash()
 
-    truck2 = Auto("Tesla", "Cybertuck", 2020, "Blue", 4)
+
+    truck2 = Truck("Tesla", "Cybertuck", 2020, "Blue", 4, bed_size='6x4')
     truck2.drive()
+    truck2.wash()
