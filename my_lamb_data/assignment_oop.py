28a9a601d256bbbb86ca974f2986c26a263deb79
Regina Dircio <reginadircio@gmail.com>
2020-05-14 19:09:07 -0500

    Oop approach with state assignment

--- my_lamb_data/assignment_oop.py
+++ my_lamb_data/assignment_oop.py
@@ -1,33 +1,37 @@
-# ASSIGNMENT 2
+# ASSIGNMENT 3
 # State abbreviation -> Full Name and visa versa. FL -> Florida, etc.
 # (Handle Washington DC and territories like Puerto Rico etc.)
 
 from pandas import DataFrame
 
+class DataProcessor():
+    def __init__(self, state_df):
+        '''
+        Parameters:
+            my_df (pandas.DataFrame) has a column called 'abbrev' with 
+            US state abbreviations.
+        '''
+        self.df = state_df
+    
 
-def add_state_names(my_df):
-    '''
-    Adds a column of US state names to a corresponding column of 
-    US state abbreviations.
-    
-    Parameters:
-        my_df (pandas.DataFrame) has a column called 'abbrev' with 
+    def add_state_names(self):
+        '''
+        Adds a column of US state names to a corresponding column of 
         US state abbreviations.
+        ''''
+
     
-    Returns:
-        Copy of the original dataframe, with another column.
-    '''
-    new_df = my_df.copy()
-    names_map = {'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut'}
-    # breakpoint()
-    new_df['name'] = new_df['abbrev'].map(names_map)
-    return new_df
+        names_map = {'CA': 'California', 'CO': 'Colorado', 'CT': 'Connecticut'}
+        # breakpoint()
+        self.df['name'] = self.df['abbrev'].map(names_map)
+        return self.df
 
 if __name__ == '__main__':
 
-    state_df = DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']})
+    df = DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']})
 
-    print(state_df.head())
+    processor = DataProcessor(df)
+    print(processor.df.head())
 
-    df_2 = add_state_names(state_df)
-    print(df_2)
+    processor.add_state_names()
+    print(processor.df.head())
