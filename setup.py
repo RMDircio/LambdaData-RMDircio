74a2cfdda24ba601dd06e2ba3068a4892768d3b9
Regina Dircio <reginadircio@gmail.com>
2020-05-14 21:17:02 -0500

    test assignment_state-test

--- setup.py
+++ setup.py
@@ -16,5 +16,5 @@ setup(
     license="MIT",
     url="https://github.com/RMDircio/LambdaData-RMDircio",
     #keywords="",
-    packages=find_packages() # ["my_lambdata"]
+    packages= ["my_lambdata"] # find_packages()
 )
---
+++ test/assignment_state_test.py
@@ -0,0 +1,21 @@
+import unittest
+from pandas import DataFrame
+from my_lamb_data.assignment_state import add_state_names
+
+
+class TestAssignment(unittest.TestCase):
+
+    def test_add_state_names(self):
+        
+        state_df = DataFrame({'abbrev': ['CA', 'CO', 'CT', 'DC', 'TX']})
+        self.assertEqual(list(state_df.columns), ['abbrev'])
+
+        result = add_state_names(state_df)
+        self.assertEqual(list(result.columns), ['abbrev', 'name'])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+# python -m test.assignment_state_test
